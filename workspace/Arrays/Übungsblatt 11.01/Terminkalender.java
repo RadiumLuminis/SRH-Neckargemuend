d56920e4a9d61f874fb9f80eda32581cc089557b
Sven Krusch <sven.krusch@web.de>
2021-01-18 12:22:30 +0100

    Unterricht vom 18.01
    Fragenblatt und Übungsblatt, Aufgaben in Eclipse

---
+++ workspace/Arrays/Übungsblatt 11.01/Terminkalender.java
@@ -0,0 +1,15 @@
+
+public class Terminkalender {
+
+	public static void main(String[] args) {
+		
+		String[] termin = {"Schwimmen", "Wandern", "Thearter", "Java lernen", "ITS anschauen", "Joggen", "Skat"};
+		
+		for(int i = 0; i < 7; i++) {
+			System.out.println(i+10 +" Uhr: " +termin[i] +"\n");	
+			
+		}	
+
+	}
+
+}
