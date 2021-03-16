09a1d7f21dca3280c21e3d81b5e4549f0e696423
Sven Krusch <sven.krusch@web.de>
2021-03-16 07:28:22 +0100

    Korrektur / Vervollständigung 01.03

---
+++ workspace/Methoden/Übungsblatt 01.03.2021/Methoden04.java
@@ -0,0 +1,16 @@
+
+public class Methoden04 {
+	
+	public static int Quadrieren (int a) {	
+		return a*a;
+	}
+	
+	public static void main(String[] args) {
+		
+		int zahl = 7;
+		
+		System.out.println("Das Quadrat der Zahl " +zahl +" lautet " +Quadrieren(zahl));	
+		
+	}
+
+}
